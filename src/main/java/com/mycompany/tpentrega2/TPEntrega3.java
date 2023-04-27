340beb41926ef1dc0dbad0da745bb8ce6c31023a
GusMEG <1986gm@gmail.com>
2023-04-27 12:08:41 -0300

    entrega final
    importacion BD y calculo puntos extra Fase

--- src/main/java/com/mycompany/tpentrega2/TPEntrega3.java
+++ src/main/java/com/mycompany/tpentrega2/TPEntrega3.java
@@ -27,7 +27,7 @@ public class TPEntrega3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
-        
+
         // Asignando variable a la ruta de acceso del archivo EQUIPOS.CSV
         String ubicacionArchivoEquipos = "C:\\tpintegradorgrupo6\\equipos.csv";
         // Trayendo el archivo EQUIPOS.CSV a través del método PATH
@@ -42,7 +42,7 @@ public class TPEntrega3 {
         //Asigno variables y rutas de acceso a resultados.csv
         String ruta2 = "C:\\tpintegradorgrupo6\\resultados.csv";
         Path archivo2 = Paths.get(ruta2);
- 
+
 //Verifica cantidad de campos del archivo resultados.csv, en caso de error, da aviso en pantalla y cierra el programa        
         for (String linea2 : Files.readAllLines(archivo2).subList(1, Files.readAllLines(archivo2).size())) {
             String[] split = linea2.split(";");
@@ -95,46 +95,54 @@ public class TPEntrega3 {
         List<Fases> faseLista = new ArrayList<>();
         //instanciando fases, 
         for (int i = 1; i <= cantidadFases; i++) {
-        Fases fase = new Fases(i);
-        faseLista.add(fase);
-       }
-//Cargo Lista de Rondas a cada Fase       
-   for (Fases iteraFase : faseLista) { 
-       List<Ronda> rondaLista2 = new ArrayList<>(); 
-       for (String linea4 : Files.readAllLines(archivo2).subList(1, Files.readAllLines(archivo2).size())) {
-            String[] split = linea4.split(";");
-           if (Integer.parseInt(split[6])==iteraFase.getNroFase()) {
-            for (Ronda iteraRonda : rondaLista) {
-        if (Integer.parseInt(split[0])==iteraRonda.getNroRonda()) {
-      rondaLista2.add(iteraRonda);
-             }}}
-        iteraFase.setRondas(rondaLista2);
-         }}
-   
-   for (Fases iteraFase : faseLista) {
-       System.out.println(iteraFase.getRondas().size());
-   }
-       
+            Fases fase = new Fases(i);
+            faseLista.add(fase);
+        }
+//Cargo Lista de Rondas a cada Fase. Verifico que la ronda no esté cargada previamentepara evitar duplicaciones.    
+        for (Fases iteraFase : faseLista) {
+            List<Ronda> rondaLista2 = new ArrayList<>();
+            for (String linea4 : Files.readAllLines(archivo2).subList(1, Files.readAllLines(archivo2).size())) {
+                String[] split = linea4.split(";");
+                if (Integer.parseInt(split[6]) == iteraFase.getNroFase()) {
+                    for (Ronda iteraRonda : rondaLista) {
+                        if (Integer.parseInt(split[0]) == iteraRonda.getNroRonda()) {
+                            boolean repetido = false;
+                            for (Ronda itera2Ronda : rondaLista2) {
+                                if (itera2Ronda.getNroRonda() == Integer.parseInt(split[0])) {
+                                    repetido = true;
+                                }
+                            }
+                            if (repetido == false) {
+                                rondaLista2.add(iteraRonda);
+                            }
+
+                        }
+                    }
+                }
+                iteraFase.setRondas(rondaLista2);
+            }
+        }
+
 // Defino variables que representan la url BD, el Usuario BD, password BD;
 //Obtengo los valores anteriores desde el archivo config.ini
-File fileToParse = new File("config.ini");
-Ini ini = new Ini(fileToParse);
-String url=ini.get("BD", "url");
-String usuario=ini.get("BD", "user");
-String pass=ini.get("BD", "pass");
+        File fileToParse = new File("config.ini");
+        Ini ini = new Ini(fileToParse);
+        String url = ini.get("BD", "url");
+        String usuario = ini.get("BD", "user");
+        String pass = ini.get("BD", "pass");
 
 //Conecto a la BD para leer los pronósticos
-Class.forName("com.mysql.cj.jdbc.Driver");
-Connection con=DriverManager.getConnection(url, usuario, pass);
-Statement stmt=con.createStatement();
-ResultSet rs=stmt.executeQuery("select * from pronosticos");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, usuario, pass);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from pronosticos");
 
 //Creando lista personas 
-List<Personas> listaPersonas = new ArrayList<>();
+        List<Personas> listaPersonas = new ArrayList<>();
 //Con la consulta SQL instanciamos Personas, verificando siempre que esa Persona no esté cargada previamente. 
-while (rs.next()){
-   boolean repetido = false;
-   for (Personas iteraPersona : listaPersonas) {
+        while (rs.next()) {
+            boolean repetido = false;
+            for (Personas iteraPersona : listaPersonas) {
                 if (iteraPersona.getNombre().equals(rs.getString(2))) {
                     repetido = true;
                 }
@@ -143,28 +151,27 @@ while (rs.next()){
                 Personas persona = new Personas(rs.getString(2));
                 listaPersonas.add(persona);
             }
-}    
+        }
 
 //Creando lista de pronósticos 
         List<Pronosticos> listaPronosticos = new ArrayList<>();
 //Con la consulta SQL  instanciamos los pronósticos y determina el resultado del mismo
-ResultSet rs2=stmt.executeQuery("select * from pronosticos");
-        while (rs2.next()){
+        ResultSet rs2 = stmt.executeQuery("select * from pronosticos");
+        while (rs2.next()) {
             if ("true".equals(rs2.getString(5))) {
-                Pronosticos pronostico = new Pronosticos(rs2.getString(2),rs2.getString(3), Resultados.ResultadosEnum.GANA_EQUIPO_1);
+                Pronosticos pronostico = new Pronosticos(rs2.getString(2), rs2.getString(3), Resultados.ResultadosEnum.GANA_EQUIPO_1);
                 listaPronosticos.add(pronostico);
             } else if ("true".equals(rs2.getString(6))) {
-                Pronosticos pronostico = new Pronosticos(rs2.getString(2),rs2.getString(3), Resultados.ResultadosEnum.EMPATE);
+                Pronosticos pronostico = new Pronosticos(rs2.getString(2), rs2.getString(3), Resultados.ResultadosEnum.EMPATE);
                 listaPronosticos.add(pronostico);
             } else if ("true".equals(rs2.getString(7))) {
-                Pronosticos pronostico = new Pronosticos(rs2.getString(2),rs2.getString(3), Resultados.ResultadosEnum.GANA_EQUIPO_2);
+                Pronosticos pronostico = new Pronosticos(rs2.getString(2), rs2.getString(3), Resultados.ResultadosEnum.GANA_EQUIPO_2);
                 listaPronosticos.add(pronostico);
             }
         }
 //Cerramos conexión a BD
-con.close();
+        con.close();
 
-       
 // Compara los objetos "pronosticos" con los objetos "partidos" y si el codigo es el mismo determina si hubo o no hubo acierto
         for (Pronosticos iteraPronosticos : listaPronosticos) {
             for (Ronda iteraRonda : rondaLista) {
@@ -196,34 +203,54 @@ con.close();
             }
             iteraPersona.setAciertosTotales(aciertosTotales);
         }
- // Declaro variable que representa la cantidad de Puntos Extra a partir del archivo de configuracion config.ini
-String puntosExtraString=ini.get("Puntos", "extra");
-int PuntosExtraConfig= Integer.parseInt(puntosExtraString);
+        // Declaro variable que representa la cantidad de Puntos Extra a partir del archivo de configuracion config.ini
+        String puntosExtraString = ini.get("Puntos", "extra");
+        int PuntosExtraConfig = Integer.parseInt(puntosExtraString);
 // Determina los Puntos Extra de un Participante que acierta todos los partidos de una Ronda
         for (Personas iteraPersona : listaPersonas) {
- int puntosExtra=0;
+            int puntosExtra = 0;
             for (Ronda iteraRonda : rondaLista) {
                 int nroRonda = iteraRonda.getNroRonda();
-                int cantidadPartidos=iteraRonda.getPartidos().size();
-           if (iteraPersona.mapAciertosRonda.get(nroRonda)==cantidadPartidos){
-          puntosExtra=puntosExtra+ PuntosExtraConfig;    
-           }
-        iteraPersona.setPuntosExtras(puntosExtra);
- }
+                int cantidadPartidos = iteraRonda.getPartidos().size();
+                if (iteraPersona.mapAciertosRonda.get(nroRonda) == cantidadPartidos) {
+                    puntosExtra = puntosExtra + PuntosExtraConfig;
+                }
+                iteraPersona.setPuntosExtras(puntosExtra);
             }
+        }
+        // Determina los Puntos Extra de un Participante que acierta todos los partidos de una Fase
+        for (Personas iteraPersona : listaPersonas) {
+            int puntosExtra = 0;
+            for (Fases iteraFases : faseLista) {
+                int cantidadPartidos = 0;
+                int cantidadAciertos = 0;
+                for (Ronda iteraRonda : iteraFases.getRondas()) {
+                    cantidadPartidos = cantidadPartidos + iteraRonda.getPartidos().size();
+                    cantidadAciertos = cantidadAciertos + iteraPersona.mapAciertosRonda.get(iteraRonda.getNroRonda());
+                }
+                if (cantidadPartidos == cantidadAciertos) {
+                    puntosExtra = puntosExtra + PuntosExtraConfig;
+                }
+            }
+            int puntosExtraTotal = iteraPersona.getPuntosExtras() + puntosExtra;
+            iteraPersona.setPuntosExtras(puntosExtraTotal);
+        }
+
 //A partir del archivo de configuracion config.ini, tomamos el valor de puntos por acierto 
-String puntosAciertoString=ini.get("Puntos", "porAcierto");
-int puntosAcierto= Integer.parseInt(puntosAciertoString);
+        String puntosAciertoString = ini.get("Puntos", "porAcierto");
+        int puntosAcierto = Integer.parseInt(puntosAciertoString);
 
 //Imprime en pantalla los participantes junto con la cantidad de aciertos y puntaje de cada ronda, el puntaje extra y el puntaje total
         for (Personas iteraPersona : listaPersonas) {
             System.out.println("El/La participante " + iteraPersona.getNombre() + " obtuvo el siguiente puntaje:");
-            for (Ronda iteraRonda : rondaLista) {
-                System.out.println("Ronda:" + iteraRonda.getNroRonda() + ", Aciertos:" + iteraPersona.mapAciertosRonda.get(iteraRonda.getNroRonda()) + ", Puntos:" + iteraPersona.calculaPuntaje(iteraPersona.mapAciertosRonda.get(iteraRonda.getNroRonda()),puntosAcierto));
-            }
-            System.out.println("Puntos Extra:"+iteraPersona.getPuntosExtras());
-            System.out.println("El puntaje total es " + (iteraPersona.getPuntosExtras()+iteraPersona.calculaPuntaje(iteraPersona.getAciertosTotales(),puntosAcierto)));
+            for (Fases iteraFase : faseLista) {
+                for (Ronda iteraRonda : iteraFase.getRondas()) {
+                    System.out.println("Fase:" + iteraFase.getNroFase() + ", Ronda:" + iteraRonda.getNroRonda() + ", Aciertos:" + iteraPersona.mapAciertosRonda.get(iteraRonda.getNroRonda()) + ", Puntos:" + iteraPersona.calculaPuntaje(iteraPersona.mapAciertosRonda.get(iteraRonda.getNroRonda()), puntosAcierto));
+                }
+                System.out.println("Puntos Extra:" + iteraPersona.getPuntosExtras());
+                System.out.println("El puntaje total es " + (iteraPersona.getPuntosExtras() + iteraPersona.calculaPuntaje(iteraPersona.getAciertosTotales(), puntosAcierto)));
 
+            }
         }
     }
 }
